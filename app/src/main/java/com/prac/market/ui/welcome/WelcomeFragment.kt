c77e6e391e8bc046a96160e34f871920f18df8b5
phliopox <91457591+phliopox@users.noreply.github.com>
2022-12-07 14:26:08 +0900

    welcome page viewpager 에 indicator 연결

--- app/src/main/java/com/prac/market/ui/welcome/WelcomeFragment.kt
+++ app/src/main/java/com/prac/market/ui/welcome/WelcomeFragment.kt
@@ -7,8 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import com.prac.market.R
 import com.prac.market.databinding.FragmentWelcomeBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_welcome.*
 
 @AndroidEntryPoint
 class WelcomeFragment : Fragment() {
@@ -25,12 +29,25 @@ class WelcomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.lifecycleOwner = viewLifecycleOwner
+        //binding.lifecycleOwner = viewLifecycleOwner
 
-        val welcomeAdapter = WelcomeAdapter()
-        binding.vpWelcome.adapter = welcomeAdapter
-        viewModel.welcomeBanner.observe(viewLifecycleOwner){
-                welcomeBanner -> welcomeAdapter.submitList(welcomeBanner)
+        /*   val welcomeAdapter = WelcomeAdapter()
+         binding.vpWelcome.adapter = welcomeAdapter
+         viewModel.welcomeBanner.observe(viewLifecycleOwner){
+                 welcomeBanner -> welcomeAdapter.submitList(welcomeBanner)
+         }*/
+
+        with(binding.vpWelcome){
+            adapter = WelcomeAdapter().apply {
+                viewModel.welcomeBanner.observe(viewLifecycleOwner){
+                    welcomeBanner -> submitList(welcomeBanner)
+                }
+            }
+            TabLayoutMediator(
+                binding.welcomeIvIndicator,
+                this)
+            {tab,position-> }.attach()
         }
+        //val findViewById = binding.root.findViewById<TabLayout>(R.id.welcome_iv_indicator)
     }
 }
