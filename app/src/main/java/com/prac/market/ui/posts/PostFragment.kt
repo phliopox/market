c8e5f262e94ccc81b41508c08860b65de611beeb
phliopox <91457591+phliopox@users.noreply.github.com>
2022-12-20 10:00:29 +0900

    error code edit

--- app/src/main/java/com/prac/market/ui/posts/PostFragment.kt
+++ app/src/main/java/com/prac/market/ui/posts/PostFragment.kt
@@ -5,17 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.prac.market.databinding.FragmentCreateBinding
+import com.prac.market.databinding.FragmentPostBinding
 
 class PostFragment : Fragment() {
 
-    private lateinit var binding : FragmentCreateBinding
+    private lateinit var binding : FragmentPostBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentCreateBinding.inflate(inflater,container,false)
+        binding = FragmentPostBinding.inflate(inflater,container,false)
         return binding.root
     }
 
